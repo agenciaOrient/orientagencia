24aa940d1ca40ecb56699bfe31cc68c78ba9f3ab
Orientagencia <110834013+agenciaOrient@users.noreply.github.com>
2022-08-22 17:24:52 -0300

    Add files via upload

---
+++ source/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+      define("MAIL",[
+        "host"=>"smtp.zoho.com",
+        "port"=> "587",
+        "username"=>"contato@orientagencia.com.br",
+        "password"=>"C@rt@123456",
+        "from_name"=>"",
+        "from_email"=>""
+    ]);
+    
+  
