d36efe7c9bc169d489c16d07fa56b2b67cadecad
Orientagencia <110834013+agenciaOrient@users.noreply.github.com>
2022-08-22 17:24:26 -0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,34 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
+
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+          event.preventDefault();
+          event.stopPropagation();
         }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
-  
-  // Animar todos os itens na tela que tiverem meu atributo data-anime
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+// Animar todos os itens na tela que tiverem meu atributo data-anime
 
 const item = document.querySelectorAll("[data-anime]");
 
 const animeScroll = () => {
-  const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85;
 
   item.forEach((element) => {
     if (windowTop > element.offsetTop) {
@@ -36,6 +41,47 @@ const animeScroll = () => {
 
 animeScroll();
 
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", () => {
   animeScroll();
-})
+});
+const btnEnviar = document.querySelector("#btn-enviar");
+
+btnEnviar.addEventListener("click", () => {
+  btnEnviarLoader.style.display = "block";
+  btnEnviar.style.display = "none";
+});
+const from_contato = document.getElementById("form_contact");
+const msgAlertErro = document.getElementById("msgAlertErroLogin");
+const msgAlert = document.getElementById("msgAlert");
+const btnEnviarLoader = document.querySelector("#btn-enviar-loader");
+from_contato.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const dadosForm = new FormData(from_contato);
+  const dados = await fetch(`./enviar_email.php`, {
+    method: "POST",
+    body: dadosForm,
+  });
+  const resposta = await dados.json();
+  console.log(resposta);
+  if (resposta["erro"]) {
+    //lert("enviado");
+    msgAlert.innerHTML = resposta["msg"];
+    // from_contato.reset()
+    setTimeout(() => {
+      document.querySelector("#msgAlert").style.display = "none";
+    }, 5000);
+    btnEnviarLoader.style.display = "none";
+    from_contato.reset()
+  } else {
+    alert("não enviou");
+    msgAlertErro.innerHTML = resposta["msg"];
+    // from_contato.reset()
+    setTimeout(() => {
+      document.querySelector("#msgAlertErro").style.display = "none";
+    }, 5000);
+    btnEnviarLoader.style.display = "none";
+    from_contato.reset()
+
+  }
+ 
+});
